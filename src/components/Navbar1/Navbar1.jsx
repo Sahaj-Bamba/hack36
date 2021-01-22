1aebb8ebe2e347fc2f1373d16de5cad0f8d2fe65
Rituparno <rituparnobiswas@gmail.com>
2021-01-22 22:48:15 +0530

    Nav links updated

--- src/components/Navbar1/Navbar1.jsx
+++ src/components/Navbar1/Navbar1.jsx
@@ -2,66 +2,92 @@ import React, { Component } from "react";
 import "./Navbar1.scss";
 
 class Navbar1 extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-	render() {
-		const { changeMenu, activeMenu } = this.props;
-		
-		return (
-			<header>
+    render() {
+        const { changeMenu, activeMenu } = this.props;
+
+        return (
+            <header>
                 <nav class="navbar">
                     <ul class="navbar-nav">
-                        
                         <li class="nav-item">
-                            <a href="#about" class="nav-link">
+                            <a
+                                href="#about"
+                                class="nav-link"
+                                onClick={(event) => changeMenu(event, "About")}
+                            >
                                 <i class="lni lni-information"></i>
                                 <span class="link-text about">About</span>
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a href="#stats" class="nav-link">
+                            <a
+                                href="#stats"
+                                class="nav-link"
+                                onClick={(event) => changeMenu(event, "Stats")}
+                            >
                                 <i class="lni lni-stats-up"></i>
                                 <span class="link-text stats">Stats</span>
                             </a>
                         </li>
-                        
+
                         <li class="nav-item">
-                            <a href="#sponsors" class="nav-link">
+                            <a
+                                href="#sponsors"
+                                class="nav-link"
+                                onClick={(event) =>
+                                    changeMenu(event, "Sponsor")
+                                }
+                            >
                                 <i class="lni lni-dollar"></i>
                                 <span class="link-text sponsors">Sponsors</span>
                             </a>
                         </li>
-                        
+
                         <li class="nav-item">
-                            <a href="#faqs" class="nav-link">
+                            <a
+                                href="#faqs"
+                                class="nav-link"
+                                onClick={(event) => changeMenu(event, "FAQ")}
+                            >
                                 <i class="lni lni-question-circle"></i>
                                 <span class="link-text faqs">FAQs</span>
                             </a>
                         </li>
-                        
+
                         <li class="nav-item">
-                            <a href="#team" class="nav-link">
+                            <a
+                                href="#team"
+                                class="nav-link"
+                                onClick={(event) => changeMenu(event, "Team")}
+                            >
                                 <i class="lni lni-network"></i>
                                 <span class="link-text team">Team</span>
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a href="#contact" class="nav-link">
+                            <a
+                                href="#contact"
+                                class="nav-link"
+                                onClick={(event) =>
+                                    changeMenu(event, "Contact")
+                                }
+                            >
                                 <i class="lni lni-phone"></i>
                                 <span class="link-text contact">Contact</span>
                             </a>
                         </li>
-
                     </ul>
                 </nav>
             </header>
-		);
-	}
+        );
+    }
 }
 
 export default Navbar1;
