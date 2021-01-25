11a8f4859aa96d6abad3959327b4acf3fbe67482
nitindoodhiya@gmail.com <n14#t20D4>
2021-01-25 19:52:44 +0530

    Team Pages added

---
+++ src/components/Pages/Team/Team.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Team.scss";
+
+class Sponsor extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+
+		return (
+			<div>
+                I am Team Member
+            </div>
+		);
+	}
+}
+
+export default Team;
