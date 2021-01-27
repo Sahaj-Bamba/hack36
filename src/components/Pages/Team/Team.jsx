5d40f7e4ec75eac654d56196a6d132819e600864
nitindoodhiya@gmail.com <n14#t20D4>
2021-01-27 21:25:15 +0530

    User 3D box added in Team Page

--- src/components/Pages/Team/Team.jsx
+++ src/components/Pages/Team/Team.jsx
@@ -10,9 +10,110 @@ class Team extends Component {
 	render() {
 
 		return (
-			<div>
-                I am Team Member
-            </div>
+			<div class="wrapper">
+			<ul class="stage">
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+			</ul> 
+			<ul class="stage">
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+				<li class="scene">
+				<div class="movie">
+    <div class="poster"></div>
+    <div class="info">
+      <header>
+        <h1>It's a Wonderful Life</h1>
+        <span class="year">1946</span>
+        <span class="rating">PG</span>
+        <span class="duration">130 minutes</span>
+      </header>
+      <p>
+        In Bedford Falls, New York on Christmas Eve, George Bailey is deeply troubled. Prayers for his well-being from friends and family reach Heaven. Clarence Odbody, Angel Second Class, is assigned to visit Earth to save George, thereby earning his wings. Franklin and Joseph, the head angels, review George's life with Clarence.
+      </p>
+    </div>
+  </div>
+				</li>
+			</ul> 
+			
+			</div>
+			
 		);
 	}
 }
