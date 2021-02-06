7bf04ba3ac40c6f30313b9d42ec26479171b5f8b
nitindoodhiya@gmail.com <n14#t20D4>
2021-02-06 22:26:05 +0530

    Contact Page added

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./components/Home/Main";
 import About from "./components/Pages/About/About";
+import Contact from "./components/Pages/Contact/Contact";
 
 class Routes extends Component {
 	constructor(props) {
@@ -29,6 +30,15 @@ class Routes extends Component {
 					render={(props) => (
 						<React.Fragment>
 							<About />
+						</React.Fragment>
+					)}
+				/>
+				<Route
+					path="/contact"
+					exact
+					render={(props) => (
+						<React.Fragment>
+							<Contact />
 						</React.Fragment>
 					)}
 				/>
---
+++ src/components/Pages/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+	return (
+			<div style={{ position: 'relative', height: '50em', width: '50em' }}>
+							<div><p>Hey</p></div>
+							</div>
+	);
+}
