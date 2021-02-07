eef665f4d868e60fbbcffe2ebac36015283f279d
nitindoodhiya@gmail.com <n14#t20D4>
2021-02-07 20:40:10 +0530

    animated milo about

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react'
 import background_aboutus from "../../../files/AboutUs/background_aboutus.svg"
 import hello_aboutus from "../../../files/AboutUs/hello_aboutus.svg"
 import milo_aboutus from "../../../files/AboutUs/milo_aboutus.svg"
+import { Component } from 'react'
+import $ from "jquery";
+class About extends Component {
+	componentDidMount() {
+		$('#background_aboutus').fadeOut(0);
+		$('#milo_about').fadeOut(0);
+		$('#hello_aboutus').fadeOut(0);
+		$('#background_aboutus').fadeIn(500,function (){
 
-export default function About() {
-
-	const [showMilo, setShowMilo] = useState(false);
-
-	return (
+			$('#milo_about').fadeIn(1000,function () {
+				$('#hello_aboutus').fadeIn(1000);
+	
+			});
+		});
+	}
+	render() {
+		return (
 			<div style={{ position: 'relative', height: '50em', width: '50em' }}>
 				<img
+					id="background_aboutus"
 					src={background_aboutus}
 					alt="background"
 					style={{
@@ -21,6 +33,7 @@ export default function About() {
 					}}
 				/>
 				<img
+					id="hello_aboutus"
 					src={hello_aboutus}
 					alt="hello about us"
 					style={{
@@ -32,6 +45,7 @@ export default function About() {
 					}}
 				/>
 				<img
+					id="milo_about"
 					className="milo_aboutus"
 					src={milo_aboutus}
 					alt="milo about us"
@@ -46,7 +60,8 @@ export default function About() {
 			</div>
 	);
 
-
-
-
+	}
 }
+
+
+export default About;
