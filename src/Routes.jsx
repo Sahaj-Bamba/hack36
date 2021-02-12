5cecc3ccafa99966236f19f5d372e92b5eca1225
nitindoodhiya@gmail.com <n14#t20D4>
2021-02-12 12:41:46 +0530

    Team Page Added

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import Main from "./components/Home/Main";
 import About from "./components/Pages/About/About";
 import Back from "./components/Pages/Back/Back";
 import Contact from "./components/Pages/Contact/Contact";
+import Team from "./components/Pages/Team/Team";
 
 class Routes extends Component {
 	constructor(props) {
@@ -43,6 +44,15 @@ class Routes extends Component {
 					render={(props) => (
 						<React.Fragment>
 							<Contact />
+						</React.Fragment>
+					)}
+				/>
+				<Route
+					path="/teams"
+					exact
+					render={(props) => (
+						<React.Fragment>
+							<Team />
 						</React.Fragment>
 					)}
 				/>
