962b52c42cdbdc61e6b0f871f06f6cc70f71c320
Sahaj Bamba <sahajbamba1999@gmail.com>
2021-03-04 19:37:12 +0530

    date fin

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -44,7 +44,7 @@ class Contact extends Component {
 		$("#seconds").html(seconds + "<span>Seconds</span>");
 
 		setInterval(() => {
-			var endTime = new Date("19 April 2021 00:00:00 GMT+01:00");
+			var endTime = new Date("9 April 2021 18:00:00 GMT+05:30");
 			endTime = Date.parse(endTime) / 1000;
 
 			var now = new Date();
