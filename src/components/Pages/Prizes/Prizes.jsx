87c159efe0a02e0bd678d4da33b36b4e6ae960df
nitindoodhiya@gmail.com <n14#t20D4>
2021-03-08 20:05:31 +0530

    Prize side added

--- src/components/Pages/Prizes/Prizes.jsx
+++ src/components/Pages/Prizes/Prizes.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "./Prizes.scss";
-
-import p1 from "../../../files/Prizes/1st.png";
-import p2 from "../../../files/Prizes/2nd.png";
-import p3 from "../../../files/Prizes/3rd.png";
-import pg from "../../../files/Prizes/BestGirls.png";
+import p1 from "../../../files/Prizes/1st-png.png";
+import p2 from "../../../files/Prizes/2nd-png.png";
+import p3 from "../../../files/Prizes/3rd-png.png";
+import podium from "../../../files/Prizes/podium-png.png";
 
 class Prizes extends Component {
 	constructor(props) {
@@ -18,29 +17,18 @@ class Prizes extends Component {
 				<section id="Prize">
 					<h1 style={{ "font-family": "Jura" }}>Prizes</h1>
 					<br />
-					<div className="Prizes">
-						<div className="PrizesCell prize1">
-							<center>
-								<img className="Prizeimg" src={p1} alt="" />
-							</center>
-						</div>
-						<div className="PrizesCell prize2">
-							<center>
-								<img className="Prizeimg" src={p2} alt="" />
-							</center>
-						</div>
-						<div className="PrizesCell prize3">
-							<center>
-								<img className="Prizeimg" src={p3} alt="" />
-							</center>
-						</div>
-						<div className="PrizesCell prizeg">
-							<center>
-								<img className="Prizeimg" src={pg} alt="" />
-							</center>
-						</div>
+					<center>						<img src={p1} className="prize1" alt="" srcset=""/>
+</center>
+					<center>
+					
+					<div className="podium">
+						<img src={podium} className="podium" alt="" srcset=""/>
 					</div>
-				</section>
+					</center>
+					<img src={p2} className="prize2" alt="" srcset=""/>
+					<img src={p3} className="prize3" alt="" srcset=""/>
+
+		</section>
 			</>
 		);
 	}
